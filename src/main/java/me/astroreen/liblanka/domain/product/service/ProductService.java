77bd7477210dac5ddeb062da5d333031e968f2b7
Ilja Zholudev <ilja.zoludev@gmail.com>
2025-02-23 21:35:37 +0200

    ProductService now can save products, however it can't because there is no such type in database, so need to fill the database with types, colors, sizes, or do so through website

--- src/main/java/me/astroreen/liblanka/domain/product/service/ProductService.java
+++ src/main/java/me/astroreen/liblanka/domain/product/service/ProductService.java
@@ -4,12 +4,10 @@ import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Min;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import me.astroreen.liblanka.domain.product.entity.Attribute;
-import me.astroreen.liblanka.domain.product.entity.ProductColor;
 import me.astroreen.liblanka.domain.product.entity.Product;
+import me.astroreen.liblanka.domain.product.entity.ProductColor;
 import me.astroreen.liblanka.domain.product.entity.ProductSize;
-import me.astroreen.liblanka.domain.product.repository.AttributeRepository;
-import me.astroreen.liblanka.domain.product.repository.ColorRepository;
+import me.astroreen.liblanka.domain.product.entity.ProductType;
 import me.astroreen.liblanka.domain.product.repository.ProductRepository;
 import me.astroreen.liblanka.domain.product.repository.SizeRepository;
 import me.astroreen.liblanka.domain.product.repository.specification.ProductSpecifications;
@@ -19,16 +17,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private @NonNull ProductRepository productRepository;
-    private @NonNull ColorRepository colorRepository;
     private @NonNull SizeRepository sizeRepository;
-    private @NonNull AttributeRepository attributeRepository;
 
     public List<Product> createProducts(@NotNull List<ProductRequest> productRequests) {
         List<Product> products = productRequests.stream().map(this::createProduct).toList();
@@ -53,45 +51,27 @@ public class ProductService {
     private Product createProduct(@NonNull ProductRequest request) {
 
         Product.ProductBuilder productBuilder = Product.builder()
+                .type(request.type)
                 .name(request.name)
                 .description(request.description)
                 .price(request.price)
-                .quantity(request.quantity);
+                .quantity(request.quantity)
+                .color(request.color);
 
-        if (request.colorId != null) {
-            Optional<ProductColor> color = colorRepository.findById(request.colorId);
-
-            if (color.isEmpty()) {
-                throw new NoSuchElementException("Provided color id does not exist in the database: " + request.colorId);
-            }
-
-            productBuilder.color(color.orElse(null));
-        }
-
-        if (request.sizeId != null) {
-            Optional<ProductSize> size = sizeRepository.findById(request.sizeId);
+        if (request.sizeName != null) {
+            Optional<ProductSize> size = sizeRepository.findByName(request.sizeName);
 
             if (size.isEmpty()) {
-                throw new NoSuchElementException("Provided size id does not exist in the database: " + request.sizeId);
+                throw new NoSuchElementException("Provided size id does not exist in the database: " + request.sizeName);
             }
 
             productBuilder.size(size.orElse(null));
         }
 
-        if (request.attributeIds != null) {
-            HashSet<Attribute> attributes = HashSet.newHashSet(request.attributeIds.size());
-
-            attributes.addAll(request.attributeIds.stream().map(attrId -> attributeRepository.findById(attrId).orElse(null)).collect(Collectors.toSet()));
-            attributes.remove(null);
-
-            productBuilder.attributes(attributes);
-        }
-
         return productBuilder.build();
     }
 
-    public record ProductRequest(@NonNull String name, @Nullable String description, @NonNull @Min(0) BigDecimal price,
-                                 @Min(0) int quantity, @Nullable Long colorId, @Nullable Long sizeId,
-                                 @Nullable Set<Long> attributeIds) {
+    public record ProductRequest(@NonNull ProductType type, @NonNull String name, @Nullable String description, @NonNull @Min(0) BigDecimal price,
+                                 @Min(0) int quantity, @Nullable ProductColor color, @Nullable String sizeName) {
     }
 }
